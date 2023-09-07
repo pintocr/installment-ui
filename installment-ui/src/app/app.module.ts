35dc8d9a130744af5165d7770f380fb6f26eefaf
endeemms2 <endee@media-saturn.com>
2023-09-07 08:12:38 +0200

    moved btn and textarea

--- installment-ui/src/app/app.module.ts
+++ installment-ui/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatComponent } from './main/chat/chat.component';
-import {buttonChat} from "./main/button/buttonChat.component";
-import {InputFieldForChat} from "./main/inputField/inputFieldForChat";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,8 +19,11 @@ import {InputFieldForChat} from "./main/inputField/inputFieldForChat";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    buttonChat,
-    InputFieldForChat
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
